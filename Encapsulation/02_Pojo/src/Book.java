56b603cad82a4ff610d1ddf626a3d39ed9910fe6
Katerina Ekaterina <mr_kiskers@mail.ru>
2022-02-22 20:42:48 +0300

    Modul 5. № 3.1.

--- Encapsulation/02_Pojo/src/Book.java
+++ Encapsulation/02_Pojo/src/Book.java
@@ -1,44 +1,33 @@
 public class Book {
 
-    private String nameBook;
-    private String author;
-    private int quantityPage;
-    private double price;
+    private final String nameBook;
+    private final String author;
+    private final int quantityPage;
+    private final double numberISNB;
 
 
-    public Book(String nameBook) {
+    public Book(String nameBook, String author, int quantityPage, double numberISNB) {
         this.nameBook = nameBook;
+        this.author = author;
+        this.quantityPage = quantityPage;
+        this.numberISNB = numberISNB;
     }
 
-    public Book(double price) {
-        this.price = price;
-    }
+
     public String getNameBook() {
         return nameBook;
     }
 
-    public void setNameBook(String nameBook) {
-        this.nameBook = nameBook;
-    }
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
     public int getQuantityPage() {
         return quantityPage;
     }
-
-    public void setQuantityPage(int quantityPage) {
-        this.quantityPage = quantityPage;
-    }
-    public double getPrice() {
-        return price;
+    public double getNumberISNB() {
+        return numberISNB;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+
+
 }
