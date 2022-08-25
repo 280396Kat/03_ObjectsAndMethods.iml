d2f93fba4c07f026080fc17842cddd737fbed731
Katerina Ekaterina <mr_kiskers@mail.ru>
2022-08-25 22:15:20 +0300

    13.2

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,14 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+
+import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +17,20 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream()
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .filter(flight -> convertDateToLocalDate(flight.getDate()).isBefore(LocalDateTime.now().plusHours(2)) &&
+                                convertDateToLocalDate(flight.getDate()).isAfter(LocalDateTime.now())))
+                                .collect(Collectors.toList());
+
+
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+    }
+    private static LocalDateTime convertDateToLocalDate(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 
 }
