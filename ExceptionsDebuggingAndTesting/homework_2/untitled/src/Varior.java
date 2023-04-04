1534aefc1e65d2e4c91a345fa9e37bd434d1612e
Katerina Ekaterina <mr_kiskers@mail.ru>
2023-04-04 20:10:44 +0300

    14.2

---
+++ ExceptionsDebuggingAndTesting/homework_2/untitled/src/Varior.java
@@ -0,0 +1,8 @@
+public class Varior {
+
+    String name;
+    int inteleg = 100;
+    int hp = 0;
+
+
+}
---
+++ ExceptionsDebuggingAndTesting/homework_2/untitled1/src/Kat.java
@@ -0,0 +1,5 @@
+public class Kat {
+    public static void main(String[] args) {
+
+    }
+}
